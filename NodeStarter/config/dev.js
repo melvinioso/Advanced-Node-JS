180ba35f16103747b6e6285a599492fd89768f5a
Matt McGavern <matt.mcgavern@gmail.com>
2022-02-07 14:11:21 -0600

    travis server configuration

--- NodeStarter/config/dev.js
+++ NodeStarter/config/dev.js
@@ -4,4 +4,5 @@ module.exports = {
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://admin:password1234@cluster0-shard-00-00.khkuy.mongodb.net:27017,cluster0-shard-00-01.khkuy.mongodb.net:27017,cluster0-shard-00-02.khkuy.mongodb.net:27017/Blogster?ssl=true&replicaSet=atlas-inheuu-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
 };
